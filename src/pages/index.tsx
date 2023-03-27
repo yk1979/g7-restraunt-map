bd270e5fa26231357aa9a1399cbe5d5ac0ee460d
p_irisawa <01029262@waku-2.com>
2023-03-27 10:39:08 +0900

    G7にピン立てした GoogleMap 追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from "next";
 
+import { Map } from "../components/Map";
 import { Sample } from "../components/Sample";
 
 const IndexPage: NextPage = () => (
   <main>
     <h1>Hello Next.js 👋</h1>
     <Sample text="銀座はカレー天国" />
+    <Map />
   </main>
 );
 
