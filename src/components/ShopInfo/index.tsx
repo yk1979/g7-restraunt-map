7ebf2d790719baf999e7e17ca9238c6908153ee2
ymnk623 <ymnk623@gmail.com>
2023-03-28 12:34:26 +0900

    buildエラーの修正　ESlintチャックを警告に変更

--- src/components/ShopInfo/index.tsx
+++ src/components/ShopInfo/index.tsx
@@ -1,28 +1,27 @@
 import { memo } from "react";
+
 // memo: 一旦アナログのデータを使用するが、後でAPI返り値が入るように変えること
 import { RESTAURANTS } from "../../mock/restaurants";
 
 import styles from "./style.module.css";
 
 const ShopInfoComponent: React.FC = () => {
-
   return (
     <div className={styles.shop_info}>
       <ul className="shop_info-list">
-
-        {RESTAURANTS.map((item,index) => (
-          <li className={styles.shop_data}>
-            <div className={styles.shop_name}>{index + 1}.&nbsp;{item.name}</div>
+        {RESTAURANTS.map((item, index) => (
+          <li key={`${item.lat}+${item.lng}`} className={styles.shop_data}>
+            <div className={styles.shop_name}>
+              {index + 1}.&nbsp;{item.name}
+            </div>
             <p className={styles.comment}>
               slackコメントがここに入りますslackコメントがここに入りますslackコメントがここに入りますslackコメントがここに入ります
             </p>
-            </li>
+          </li>
         ))}
-
       </ul>
     </div>
   );
-
 };
 
 export const ShopInfo = memo(ShopInfoComponent);
