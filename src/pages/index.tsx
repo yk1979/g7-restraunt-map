5e1b4866f6036a4827ed17fcfce7a7bc5ddd232e
reo-morisawa <reo.morisawa@nijibox.co.jp>
2023-03-28 15:23:32 +0900

    ページでapiからの情報を取得
    - getServerSidePropsを定義
      - slackHistories: slackからとってきた生データ
      - restaurantsData: レストランのデータ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,86 @@ import { Map } from "../components/Map";
 import { ShopInfo } from "../components/ShopInfo";
 import { Sample } from "../components/Sample";
 
-const IndexPage: NextPage = () => (
-  <main>
-    <h1>Hello Next.js 👋</h1>
-    <Sample text="銀座はカレー天国" />
-    <Map />
-    <ShopInfo />
-  </main>
-);
+type SlackHistories = any[];
+
+type Props = {
+  slackHistories: SlackHistories;
+  restaurantsData: any;
+};
+
+type SlackData = {
+  user: string;
+  name: string;
+  address: string;
+  comment: string;
+  url: string;
+};
+
+const IndexPage: NextPage<Props> = ({ slackHistories, restaurantsData }) => {
+  return (
+    <main>
+      <h1>Hello Next.js 👋</h1>
+      <Sample text="銀座はカレー天国" />
+      <Map />
+      <ShopInfo />
+    </main>
+  );
+};
 
 export default IndexPage;
+
+const getSlackHistories = async (ctx: any) => {
+  try {
+    const host = ctx.req.headers.host || "localhost:3000";
+    const protocol = /^localhost/.test(host) ? "http" : "https";
+    const jsonData = await fetch(
+      `${protocol}://${host}/api/slack_history`,
+    ).then((data) => data.json());
+    console.log("gSSP", { jsonData });
+    return jsonData;
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
+
+const getAttachmentsInfoFromMessage = (history: any) => {
+  if (
+    !history ||
+    !Array.isArray(history.attachments) ||
+    !history.attachments[0]
+  ) {
+    return { url: "", name: "" };
+  }
+  return {
+    url: history.attachments[0].from_url || "",
+    name: history.attachments[0].title || "",
+  };
+};
+
+const convertData = (slackHistories: SlackHistories): SlackData[] => {
+  return slackHistories
+    .filter((history) => history.subtype !== "channel_join")
+    .map((history) => {
+      const { url, name } = getAttachmentsInfoFromMessage(history);
+      console.log();
+      return {
+        name,
+        url,
+        user: history.user,
+        address: "",
+        comment: history.text,
+      };
+    });
+};
+
+export const getServerSideProps = async (ctx: any) => {
+  const slackHistories = await getSlackHistories(ctx);
+  const restaurantsData = convertData(slackHistories);
+  return {
+    props: {
+      slackHistories,
+      restaurantsData,
+    },
+  };
+};
